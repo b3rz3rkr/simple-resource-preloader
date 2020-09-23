9dba50e3f4e6a09e19545d5fa42b3e5dec20c7b8
Serhiy <22635858+b3rz3rkr@users.noreply.github.com>
2020-09-23 09:13:55 +0300

    example usage update

--- example.js
+++ example.js
@@ -1,18 +1,22 @@
+//This file shows all possible options, check documentation for default values
 const
-    SimpleResourcesPreloader = require('./SimpleResourcePreloader'),
-    preloader = new SimpleResourcesPreloader({
+    SRPreloader = require('simple-resource-preloader'),
+    preloader = new SRPreloader({
         debug: true, //use to track functions order inside class
         events: true, //triggering events during caching
         eventsTarget: document, //any DOM element to listen events
-        eventName: 'preloaderDone', //triggering this even if all files cached successfully
+        eventEnd: 'preloaderDone', //triggering this even if all files cached successfully
         eventError: 'preloaderError', //triggering this even if we have an errors
-        eventPercent: 'preloaderPercents', //triggering this even on percentage changes
+        eventProgress: 'preloaderPercents', //triggering this even on percentage changes
         files: ['./video/intro.mp4', './video/background.mp4'], //files for caching
         callback: str => console.log(str), //callback function to execute when files cached
         cbParams: ['run custom callback'], //params for callback function
-        onError: (error, str) => console.log(str + ' ' + error), //callback function to execute if we have errors
-        onErrorParams: ['Something went wrong.'], //params for errors callback function
-        onPercent: (value, str1, str2) => console.log(`${str1} ${str2} ${value}%`),  //callback function to execute on percentage changes
-        onPercentParams: ['Custom percent callback.', 'Loaded '], //params for percentage callback function
+        ifError: (error, str) => console.log(str + ' ' + error), //callback function to execute if we have errors
+        ifErrorParams: ['Something went wrong.'], //params for errors callback function
+        onProgress: (value, str1, str2) => console.log(`${str1} ${str2} ${value}%`),  //callback function to execute on percentage changes
+        onProgressParams: ['Custom percent callback.', 'Loaded '], //params for percentage callback function
+        preloader: '#myPreloader', //this preloader can be hidden with default callbacks and methods
+        progress: '#myPreloader [progressbars]', //updates of progress attribute during loading
+        writePercentsAttr: 'progressbars' //updates inner text during loading for all progress elements with this attribute
     });
 preloader.preload();
