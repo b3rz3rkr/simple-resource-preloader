f7a4fc5506b5410d7e55779b84099d92b0f1a1ad
Serhiy <22635858+b3rz3rkr@users.noreply.github.com>
2020-09-25 12:54:05 +0300

    prepeared to private class fields

--- SimpleResourcePreloader.js
+++ SimpleResourcePreloader.js
@@ -1,4 +1,7 @@
 module.exports = class SimpleResourcePreloader {
+/*
+    __methods will be switched to #methods when private class fields support will be added to webpack
+ */
     constructor(options) {
         this.options = {...this.defaultOptions, ...options};
         this.loaded = 0;
@@ -10,7 +13,7 @@ module.exports = class SimpleResourcePreloader {
     get defaultOptions() {
         return {
             callback: () => {
-                this.log('run default callback');
+                this.__log('run default callback');
                 this.hidePreloader();
             },
             cbParams: [],
@@ -22,48 +25,48 @@ module.exports = class SimpleResourcePreloader {
             eventsTarget: document,
             files: [],
             ifError: () => {
-                this.log('run default error callback');
+                this.__log('run default error callback');
                 this.hidePreloader();
             },
             ifErrorParams: [],
             onProgress: () => {
-                this.log('run default progress callback');
+                this.__log('run default progress callback');
                 this.updateProgress();
             },
             onProgressParams: [],
-            preloader: document.querySelector('#preloader'),
-            progress: document.querySelectorAll('#preloader [progress]'),
+            preloader: document.querySelector('__preloader'),
+            progress: document.querySelectorAll('__preloader [progress]'),
             writePercentsAttr: 'txt-progress'
         };
     }
 
     preload() {
-        this.log('start preload');
-        if (this.validate()) {
-            this.files.forEach(uri => this.load(uri));
+        this.__log('start preload');
+        if (this.__validate()) {
+            this.files.forEach(uri => this.__load(uri));
         }
     }
 
-    validate() {
-        this.log('validate');
-        if (this.count) {
+    __validate() {
+        this.__log('validate');
+        if (this.__count) {
             return true;
         } else {
             this.error = new Error('No files to preload');
-            this.runError();
-            this.exitPreloader();
+            this.__runError();
+            this.__exitPreloader();
             return false;
         }
     }
 
-    load(uri) {
-        this.log(`load ${uri}`);
+    __load(uri) {
+        this.__log(`load ${uri}`);
         const xhr = new XMLHttpRequest();
         xhr.onprogress = e => {
-            this.updateSizes(e, uri);
-            const percents = this.percentageCalc();
+            this.__updateSizes(e, uri);
+            const percents = this.__percentageCalc();
             if (percents !== this.percents) {
-                const details = this.getDetails({value: percents});
+                const details = this.__getDetails({value: percents});
                 this.percents = percents;
                 this.triggerEvent(this.options.eventProgress, details);
                 if (typeof this.onProgress === 'function') {
@@ -79,37 +82,37 @@ module.exports = class SimpleResourcePreloader {
             } else {
                 this.noAccess++;
                 this.error = new Error(`Can't Access "${uri}". Status code ${target.status}`);
-                this.runError();
+                this.__runError();
             }
-            if (this.loaded + this.noAccess === this.count) {
-                const details = this.getDetails();
+            if (this.loaded + this.noAccess === this.__count) {
+                const details = this.__getDetails();
                 this.triggerEvent(this.options.eventEnd, details);
-                this.exitPreloader();
+                this.__exitPreloader();
             }
         };
 
         xhr.ontimeout = () => {
             this.error = new Error(`timeout error`);
-            this.runError();
+            this.__runError();
         };
 
         xhr.open('GET', uri, true);
         xhr.send();
     }
 
-    updateSizes(e, uri) {
+    __updateSizes(e, uri) {
         this.fileSizes[uri] = {
             size: e.total,
             loaded: e.loaded
         };
     }
 
-    percentageCalc() {
+    __percentageCalc() {
         const keys = Object.keys(this.fileSizes),
             count = this.options.files.length;
 
         if (keys.length === count) {
-            this.log('calc percents');
+            this.__log('calc percents');
             let total = 0,
                 loaded = 0;
 
@@ -133,21 +136,8 @@ module.exports = class SimpleResourcePreloader {
         return 0;
     }
 
-    triggerEvent(eventName, details) {
-        if (this.options.events === true || this.options.events === 'true') {
-            const
-                eventDetails = details || {},
-                event = new CustomEvent(eventName, {detail: eventDetails}),
-                target = this.getElement(this.options.eventsTarget);
-            if (target) {
-                this.log(`trigger event ${eventName}`);
-                target.dispatchEvent(event);
-            }
-        }
-    }
-
-    exitPreloader() {
-        this.log(`exit preloader ${this.error ? 'with error' : 'without errors'}`);
+    __exitPreloader() {
+        this.__log(`exit preloader ${this.error ? 'with error' : 'without errors'}`);
         let callback, cbParams;
 
         if (this.error) {
@@ -168,8 +158,8 @@ module.exports = class SimpleResourcePreloader {
         }
     }
 
-    getElement(element, all) {
-        this.log(`getting element type is ${typeof element}`);
+    __getElement(element, all) {
+        this.__log(`getting element type is ${typeof element}`);
         if (typeof element === 'string') {
             if (all) {
                 element = document.querySelectorAll(element);
@@ -180,12 +170,12 @@ module.exports = class SimpleResourcePreloader {
         if (element instanceof HTMLElement || element instanceof HTMLDocument || element instanceof NodeList) {
             return element;
         }
-        this.log('element not found');
-        this.log(element);
+        this.__log('element not found');
+        this.__log(element);
         return false;
     }
 
-    getDetails(details) {
+    __getDetails(details) {
         const common = {
             loaded: this.loaded,
             failed: this.noAccess
@@ -193,23 +183,36 @@ module.exports = class SimpleResourcePreloader {
         return {...common, ...details};
     }
 
+    triggerEvent(eventName, details) {
+        if (this.options.events === true || this.options.events === 'true') {
+            const
+                eventDetails = details || {},
+                event = new CustomEvent(eventName, {detail: eventDetails}),
+                target = this.__getElement(this.options.eventsTarget);
+            if (target) {
+                this.__log(`trigger event ${eventName}`);
+                target.dispatchEvent(event);
+            }
+        }
+    }
+
     hidePreloader() {
         if (this.preloader) {
-            this.log('hide preloader');
+            this.__log('hide preloader');
             this.preloader.style.display = 'none';
         }
     }
 
     showPreloader() {
         if (this.preloader) {
-            this.log('show preloader');
+            this.__log('show preloader');
             this.preloader.style.display = '';
         }
     }
 
     updateProgress() {
         if (this.progress) {
-            this.log('updateProgress');
+            this.__log('updateProgress');
             this.progress.forEach(node => {
                 if (node.hasAttribute(this.writePercentsAttr)) {
                     node.textContent = `${this.percents}%`;
@@ -221,123 +224,123 @@ module.exports = class SimpleResourcePreloader {
         }
     }
 
-    runError() {
-        console.log(`%c${this.error.message}`, 'color: #bb5577');
-        const details = this.getDetails({error: this.error.message});
+    get preloader() {
+        this.__log('get preloader element');
+        return this.__getElement(this.options.preloader);
+    }
+
+    set preloader(element) {
+        this.__log('set preloader element');
+        this.options.preloader = element;
+    }
+
+    get progress() {
+        this.__log('get progress element');
+        return this.__getElement(this.options.progress, true);
+    }
+
+    set progress(element) {
+        this.__log('set progress element');
+        this.options.progress = element;
+    }
+
+    get writePercentsAttr() {
+        this.__log('get attribute to show progress');
+        return this.options.writePercentsAttr;
+    }
+
+    set writePercentsAttr(attr) {
+        this.__log('set attribute to show progress');
+        this.options.writePercentsAttr = attr;
+    }
+
+    get files() {
+        this.__log('get files');
+        return this.options.files;
+    }
+
+    set files(files) {
+        this.__log('set files');
+        this.options.files = files;
+    }
+
+    __runError() {
+        console.log(`%c${this.error.message}`, 'color: __bb5577');
+        const details = this.__getDetails({error: this.error.message});
         this.triggerEvent(this.options.eventError, details);
     }
 
-    log(message) {
+    __log(message) {
         if (this.options.debug === true || this.options.debug === 'true') {
             console.log(message);
         }
     }
 
-    get preloader() {
-        this.log('get preloader element');
-        return this.getElement(this.options.preloader);
-    }
-
-    set preloader(element) {
-        this.log('set preloader element');
-        this.options.preloader = element;
-    }
-
-    get progress() {
-        this.log('get progress element');
-        return this.getElement(this.options.progress, true);
-    }
-
-    set progress(element) {
-        this.log('set progress element');
-        this.options.progress = element;
-    }
-
-    get writePercentsAttr() {
-        this.log('get attribute to show progress');
-        return this.options.writePercentsAttr;
-    }
-
-    set writePercentsAttr(attr) {
-        this.log('set attribute to show progress');
-        this.options.writePercentsAttr = attr;
-    }
-
-    get count() {
-        this.log('get total');
+    get __count() {
+        this.__log('get total');
         return this.options.files.length;
-    }
-
-    get files() {
-        this.log('get files');
-        return this.options.files;
-    }
-
-    set files(files) {
-        this.log('set files');
-        this.options.files = files;
     }
 
     //callbacks
     get callback() {
-        this.log('get callback');
+        this.__log('get callback');
         return this.options.callback;
     }
 
     set callback(callback) {
-        this.log('set callback');
+        this.__log('set callback');
         this.options.callback = callback;
     }
 
     get cbParams() {
-        this.log('get cbParams');
+        this.__log('get cbParams');
         return this.options.cbParams;
     }
 
     set cbParams(params) {
-        this.log('set cbParams');
+        this.__log('set cbParams');
         this.options.cbParams = params;
     }
 
     //error callback
     get ifError() {
-        this.log('get error callback');
+        this.__log('get error callback');
         return this.options.ifError;
     }
 
     set ifError(ifError) {
-        this.log('set error callback');
+        this.__log('set error callback');
         this.options.ifError = ifError;
     }
 
     get ifErrorParams() {
-        this.log('get error cbParams');
+        this.__log('get error cbParams');
         return this.options.ifErrorParams;
     }
 
     set ifErrorParams(params) {
-        this.log('set error cbParams');
+        this.__log('set error cbParams');
         this.options.ifErrorParams = params;
     }
 
     //percents changing callback
     get onProgress() {
-        this.log('get progress callback');
+        this.__log('get progress callback');
         return this.options.onProgress;
     }
 
     set onProgress(onProgress) {
-        this.log('set progress callback');
+        this.__log('set progress callback');
         this.options.onProgress = onProgress;
     }
 
     get onProgressParams() {
-        this.log('get progress cbParams');
+        this.__log('get progress cbParams');
         return this.options.onProgressParams;
     }
 
     set onProgressParams(params) {
-        this.log('set progress cbParams');
+        this.__log('set progress cbParams');
         this.options.onProgressParams = params;
     }
 };
