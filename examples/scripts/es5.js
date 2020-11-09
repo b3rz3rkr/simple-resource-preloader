1804c7d47f9eb29c7add552ef231838930b645e8
Serhiy <22635858+b3rz3rkr@users.noreply.github.com>
2020-11-09 09:03:42 +0200

    updated examples with speed values

--- examples/scripts/es5.js
+++ examples/scripts/es5.js
@@ -1,9 +1,23 @@
 var preloader = new SimpleResourcePreloader({
-    files: ['https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4'],
-    callback: function (str) {console.log(str)},
-    cbParams: ['run custom callback'],
-    ifError: function (str) {console.log(str)},
-    ifErrorParams:['Error cb'],
-    debug: true
-});
+        files: ['https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4'],
+        callback: function (str) {
+            console.log(str);
+        },
+        cbParams: ['run custom callback'],
+        ifError: function (str) {
+            console.log(str);
+        },
+        ifErrorParams: ['Error cb'],
+        debug: true
+    }),
+    progressOldFunction = preloader.onProgress,
+    speedContainer = document.querySelector('.speed'),
+    speedValueContainer = speedContainer.querySelector('.value'),
+    speedUnitsContainer = speedContainer.querySelector('.units');
+
+preloader.onProgress = function (percents, speed) {
+    progressOldFunction();
+    speedValueContainer.textContent = speed.value;
+    speedUnitsContainer.textContent = speed.units;
+};
 preloader.preload();
--- examples/scripts/es6+min.js
+++ examples/scripts/es6+min.js
@@ -1,11 +1,21 @@
 import SRPreloader from '../../dist/SimpleResourcePreloader.min.js';
 
 const preloader = new SRPreloader({
-    files: ['https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4'],
-    callback: function (str) {console.log(str)},
-    cbParams: ['run custom callback'],
-    ifError: function (str) {console.log(str)},
-    ifErrorParams:['Error cb'],
-    debug: true
-});
+        files: ['https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4'],
+        callback: str => console.log(str),
+        cbParams: ['run custom callback'],
+        ifError: str => console.log(str),
+        ifErrorParams: ['Error cb'],
+        debug: true
+    }),
+    progressOldFunction = preloader.onProgress,
+    speedContainer = document.querySelector('.speed'),
+    speedValueContainer = speedContainer.querySelector('.value'),
+    speedUnitsContainer = speedContainer.querySelector('.units');
+
+preloader.onProgress = (percents, speed) => {
+    progressOldFunction();
+    speedValueContainer.textContent = speed.value;
+    speedUnitsContainer.textContent = speed.units;
+};
 preloader.preload();
